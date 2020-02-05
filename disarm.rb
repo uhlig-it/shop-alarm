ca62f9f7f8210d06b3b329115317bb1a6a19664e
Steffen Uhlig <steffen@familie-uhlig.net>
2020-02-05 12:49:48 +0100

    Do not send if buffer is empty

--- disarm.rb
+++ disarm.rb
@@ -21,51 +21,82 @@ class Buffer
   end
 end
 
+class AppendCommand
+  def initialize(char)
+    @char = char
+  end
+
+  def call(buffer)
+    buffer.append(@char)
+  end
+end
+
+class SendCommand
+  def initialize(mqtt)
+    @mqtt = mqtt
+  end
+
+  def call(buffer)
+    return if buffer.empty?
+    warn "Sending #{buffer}"
+    buffer.reset
+  end
+end
+
+class PrintCommand
+  def call(buffer)
+    if buffer.empty?
+      warn 'Current buffer is empty'
+    else
+      warn "Current buffer is #{buffer}"
+    end
+  end
+end
+
+class ChopCommand
+  def call(buffer)
+    buffer.chop!
+  end
+end
+
 class Keypad
   ACTIONS = {
-    :KEY_KP0        => '0',
-    :KEY_KP1        => '1',
-    :KEY_KP2        => '2',
-    :KEY_KP3        => '3',
-    :KEY_KP4        => '4',
-    :KEY_KP5        => '5',
-    :KEY_KP6        => '6',
-    :KEY_KP7        => '7',
-    :KEY_KP8        => '8',
-    :KEY_KP9        => '9',
-    :KEY_KPPLUS     => '+',
-    :KEY_KPMINUS    => '-',
-    :KEY_KPASTERISK => '*',
-    :KEY_KPSLASH    => '/',
-    :KEY_KPDOT      => '.',
-    :KEY_NUMLOCK    => lambda { |buffer| warn "Current buffer is #{buffer}" },
-    :KEY_KPENTER    => lambda { |buffer| warn "Sending #{buffer}"; buffer.reset },
-    :KEY_BACKSPACE  => lambda { |buffer| buffer.chop! },
+    :KEY_KP0        => AppendCommand.new('0'),
+    :KEY_KP1        => AppendCommand.new('1'),
+    :KEY_KP2        => AppendCommand.new('2'),
+    :KEY_KP3        => AppendCommand.new('3'),
+    :KEY_KP4        => AppendCommand.new('4'),
+    :KEY_KP5        => AppendCommand.new('5'),
+    :KEY_KP6        => AppendCommand.new('6'),
+    :KEY_KP7        => AppendCommand.new('7'),
+    :KEY_KP8        => AppendCommand.new('8'),
+    :KEY_KP9        => AppendCommand.new('9'),
+    :KEY_KPPLUS     => AppendCommand.new('+'),
+    :KEY_KPMINUS    => AppendCommand.new('-'),
+    :KEY_KPASTERISK => AppendCommand.new('*'),
+    :KEY_KPSLASH    => AppendCommand.new('/'),
+    :KEY_KPDOT      => AppendCommand.new('.'),
+    :KEY_NUMLOCK    => PrintCommand.new,
+    :KEY_KPENTER    => SendCommand.new(nil),
+    :KEY_BACKSPACE  => ChopCommand.new,
   }
 
   def initialize(device)
     @keyboard = Evdev.new(device)
+    @buffer = Buffer.new
 
     @keyboard.on(*ACTIONS.keys) do |state, key|
       case state
-      when 0
-        action = ACTIONS[key]
-
-        if action.respond_to?(:call)
-          action.call(@buffer)
-        else
-          @buffer.append(action)
-        end
-      when 1
-        # warn "Pressed #{ACTIONS[key]}"
-      when 2
-        # warn "Woah, slow down with that #{key}!"
+      when 0 # released
+        ACTIONS[key].call(@buffer)
+      when 1 # pressed
+        # ignored
+      when 2 # repeated
+        # ignored
       else
-        raise "What? #{state}"
+        raise "Don't know what to do with state #{state}"
       end
     end
-
-    @buffer = Buffer.new
   end
 
   def start!
@@ -92,7 +123,6 @@ __END__
 TODO
 
 * Timeout
-* Do not send empty buffer
 * Command line args
 * MQTT
 * Tests?
